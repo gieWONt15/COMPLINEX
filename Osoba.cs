fe1847a410f8b85a36e37ade357934778ad28b4b
gieWONt15 <pawdyslaw@gmail.com>
2025-05-06 21:48:50 +0200

    refactor: change Osoba class to abstract and add properties and methods

--- Osoba.cs
+++ Osoba.cs
@@ -1,6 +1,20 @@
 ﻿namespace COMPLINEX;
 
-public class Osoba
+public abstract class Osoba
 {
-    
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public DateTime DataUrodzenia { get; set; }
+
+    protected Osoba(string imie, string nazwisko, DateTime dataUrodzenia)
+    {
+        Imie = imie;
+        Nazwisko = nazwisko;
+        DataUrodzenia = dataUrodzenia;
+    }
+
+    public virtual void PrzedstawSie()
+    {
+        Console.WriteLine($"Nazywam się {Imie} {Nazwisko}.");
+    }
 }
