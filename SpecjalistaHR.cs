a43501766c065519529c146a710985840ed79503
Paweł <pawdyslaw@gmail.com>
2025-05-08 10:21:40 +0200

    feat: add SpecjalistaHR class with recruitment and specialization properties

---
+++ SpecjalistaHR.cs
@@ -0,0 +1,30 @@
+namespace COMPLINEX;
+
+public class SpecjalistaHR : Pracownik
+{
+    public int LiczbaZrekrutowanychPracownikow { get; set; }
+    public string ObszarSpecjalizacji { get; set; }
+
+    public SpecjalistaHR(string imie, string nazwisko, DateTime dataUrodzenia, string numerPracownika,
+        DateTime dataZatrudnienia, decimal wynagrodzenie, int liczbaZrekrutowanychPracownikow, string obaObszarSpecjalizacji) : base(imie, nazwisko, dataUrodzenia, numerPracownika, dataZatrudnienia, wynagrodzenie)
+    {
+        LiczbaZrekrutowanychPracownikow = liczbaZrekrutowanychPracownikow;
+        ObszarSpecjalizacji = obaObszarSpecjalizacji;
+    }
+
+    public override void PrzedstawSie()
+    {
+        base.PrzedstawSie();
+        Console.WriteLine($"Jestem specjalistą HR specjalizującym się w {ObszarSpecjalizacji}.");
+    }
+    
+    public override void OpisObowiazkow()
+    {
+        Console.WriteLine("Jako specjalista HR odpowiadam za rekrutację, onboarding nowych pracowników, prowadzenie dokumentacji pracowniczej, organizację szkoleń oraz wsparcie w sprawach związanych z zasobami ludzkimi.");
+    }
+    
+    public override decimal ObliczPremie()
+    {
+        return Wynagrodzenie * 0.08m + (LiczbaZrekrutowanychPracownikow * 200);
+    }
+}
