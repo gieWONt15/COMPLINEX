21c0d5c5cdef14d02106fbe24657f33deaf8604d
Paweł <pawdyslaw@gmail.com>
2025-05-11 15:52:23 +0200

    feat: implement employee evaluation system with detailed ratings for various roles

--- Program.cs
+++ Program.cs
@@ -1,91 +1,119 @@
 ﻿namespace COMPLINEX;
 
+// Przykład użycia w Program.cs
 class Program
 {
     static void Main()
     {
-        // Tworzenie rejestru pracowników
-        var rejestrPracownikow = new RejestrPracownikow();
-        
-        // Tworzenie pracowników różnych typów
-        var inzynier1 = new InzynierProdukcji(
+        // Tworzenie pracowników
+        var inzynier = new InzynierProdukcji(
             "Jan", "Kowalski", new DateTime(1980, 5, 15),
             "INZ001", new DateTime(2010, 6, 1), 8000m,
             "automatyzacja procesów", 3);
-            
-        var inzynier2 = new InzynierProdukcji(
-            "Anna", "Nowak", new DateTime(1985, 8, 20),
-            "INZ002", new DateTime(2015, 3, 15), 7500m,
-            "kontrola jakości", 2);
             
         var inzynierKierownik = new InzynierKierownikProjektu(
             "Tomasz", "Wiśniewski", new DateTime(1978, 11, 5),
             "INZ003", new DateTime(2008, 4, 10), 9500m,
             "optymalizacja produkcji", 4, 2);
             
-        var kierownikProdukcji = new KierownikProdukcjiProjektu(
-            "Maria", "Dąbrowska", new DateTime(1975, 3, 18),
-            "KP001", new DateTime(2005, 7, 1), 12000m,
-            12, "Produkcja", 110.5, 3);
-            
         var handlowiec = new PrzedstawicielHandlowy(
             "Piotr", "Zieliński", new DateTime(1988, 9, 25),
             "PH001", new DateTime(2017, 2, 1), 6000m,
             150000m, 0.02);
+            
+        var kierownik = new KierownikProdukcji(
+            "Maria", "Dąbrowska", new DateTime(1975, 3, 18),
+            "KP001", new DateTime(2005, 7, 1), 12000m,
+            12, "Produkcja", 110.5);
             
         var specjalistaHr = new SpecjalistaHR(
             "Katarzyna", "Lewandowska", new DateTime(1990, 7, 12),
             "HR001", new DateTime(2018, 9, 1), 6500m,
             15, "rekrutacja i onboarding");
             
-        // Dodawanie pracowników do rejestru
-        rejestrPracownikow.DodajPracownika(inzynier1);
-        rejestrPracownikow.DodajPracownika(inzynier2);
-        rejestrPracownikow.DodajPracownika(inzynierKierownik);
-        rejestrPracownikow.DodajPracownika(kierownikProdukcji);
-        rejestrPracownikow.DodajPracownika(handlowiec);
-        rejestrPracownikow.DodajPracownika(specjalistaHr);
+        // Tworzenie systemu ocen
+        var systemOcen = new SystemOcenPracowniczych();
         
-        // Tworzenie rejestru projektów
-        var rejestrProjektow = new RejestrProjektow();
+        // Tworzenie ocen dla różnych typów pracowników
         
-        // Tworzenie projektów
-        var projektA = new Projekt(
-            "Optymalizacja linii produkcyjnej A", 
-            "Projekt mający na celu zwiększenie efektywności linii produkcyjnej A o 20%",
-            new DateTime(2023, 1, 15),
-            new DateTime(2023, 6, 30));
+        // Ocena dla inżyniera
+        var ocenaSzczegolowaInzyniera = new Dictionary<string, int>
+        {
+            {"Efektywnosc", 4},
+            {"Innowacyjnosc", 5},
+            {"JakoscPracy", 4},
+            {"RealizacjaProjektow", 3}
+        };
+        
+        var ocenaInzyniera = FabrykaOcen.UtworzOcene(
+            inzynier, 2023, specjalistaHr, 
+            "Wybitny specjalista, wprowadził kilka innowacyjnych rozwiązań w procesie produkcji.",
+            ocenaSzczegolowaInzyniera);
             
-        var projektB = new Projekt(
-            "Wdrożenie systemu kontroli jakości", 
-            "Implementacja nowego systemu kontroli jakości zgodnego z ISO 9001:2015",
-            new DateTime(2023, 4, 1));
+        // Ocena dla inżyniera kierownika projektu
+        var ocenaSzczegolowaInzynieraKierownika = new Dictionary<string, int>
+        {
+            {"EfektywnoscPracy", 4},
+            {"ZarzadzanieProjektami", 5},
+            {"UmiejetnosciTechniczne", 4},
+            {"ZarzadzanieZespolem", 4}
+        };
+        
+        var ocenaInzynieraKierownika = FabrykaOcen.UtworzOcene(
+            inzynierKierownik, 2023, specjalistaHr, 
+            "Skutecznie łączy umiejętności techniczne z zarządzaniem zespołem. Projekty realizowane terminowo.",
+            ocenaSzczegolowaInzynieraKierownika);
             
-        // Ustawienie kierowników projektów
-        projektA.UstawKierownika(kierownikProdukcji);
-        projektB.UstawKierownika(inzynierKierownik);
+        // Ocena dla handlowca
+        var ocenaSzczegolowaHandlowca = new Dictionary<string, int>
+        {
+            {"RealizacjaCelowSprzedazowych", 5},
+            {"PozyskiwanieKlientow", 4},
+            {"ObslugaKlienta", 3},
+            {"UmiejetnosciNegocjacyjne", 5}
+        };
         
-        // Dodawanie członków zespołu
-        projektA.DodajCzlonkaZespolu(inzynier1);
-        projektA.DodajCzlonkaZespolu(handlowiec);
+        var ocenaHandlowca = FabrykaOcen.UtworzOcene(
+            handlowiec, 2023, specjalistaHr, 
+            "Przekroczył cele sprzedażowe o 15%. Bardzo dobre umiejętności negocjacyjne.",
+            ocenaSzczegolowaHandlowca);
+            
+        // Ocena dla kierownika
+        var ocenaSzczegolowaKierownika = new Dictionary<string, int>
+        {
+            {"ZarzadzanieZespolem", 4},
+            {"RealizacjaCelow", 5},
+            {"UmiejetnosciOrganizacyjne", 4},
+            {"Innowacyjnosc", 3}
+        };
         
-        projektB.DodajCzlonkaZespolu(inzynier2);
-        projektB.DodajCzlonkaZespolu(specjalistaHr);
+        var ocenaKierownika = FabrykaOcen.UtworzOcene(
+            kierownik, 2023, specjalistaHr, 
+            "Efektywnie zarządza zespołem, terminowo realizuje cele produkcyjne. Wysoka kultura osobista.",
+            ocenaSzczegolowaKierownika);
+            
+        // Ocena dla specjalisty HR
+        var ocenaSzczegolowaSpecjalistyHr = new Dictionary<string, int>
+        {
+            {"EfektywnoscRekrutacji", 5},
+            {"Onboarding", 4},
+            {"ZarządzanieTalentami", 4},
+            {"UmiejetnosciInterpersonalne", 5}
+        };
         
-        // Dodawanie projektów do rejestru
-        rejestrProjektow.DodajProjekt(projektA);
-        rejestrProjektow.DodajProjekt(projektB);
+        var ocenaSpecjalistyHr = FabrykaOcen.UtworzOcene(
+            specjalistaHr, 2023, specjalistaHr, 
+            "Doskonała skuteczność w rekrutacji i onboardingu nowych pracowników. Bardzo dobre relacje z pracownikami.",
+            ocenaSzczegolowaSpecjalistyHr);
+            
+        // Dodawanie ocen do systemu ocen
+        systemOcen.DodajOcene(ocenaInzyniera);
+        systemOcen.DodajOcene(ocenaInzynieraKierownika);
+        systemOcen.DodajOcene(ocenaHandlowca);
+        systemOcen.DodajOcene(ocenaKierownika);
+        systemOcen.DodajOcene(ocenaSpecjalistyHr);
         
-        // Wyświetlanie informacji o projektach
-        rejestrProjektow.WyswietlWszystkieProjekty();
-        rejestrProjektow.WyswietlSzczegolyProjektow(rejestrProjektow.PobierzWszystkieProjekty());
-        
-        // Demonstracja funkcji kierownika projektu
-        Console.WriteLine("\n=== DEMONSTRACJA FUNKCJI KIEROWNIKA PROJEKTU ===");
-        kierownikProdukcji.ZarzadzajProjektem();
-        kierownikProdukcji.PrzeprowadzEwaluacjeProjektu();
-        
-        inzynierKierownik.ZarzadzajProjektem();
-        inzynierKierownik.PrzeprowadzEwaluacjeProjektu();
+        // Wyświetlanie ocen
+        systemOcen.WyswietlWszystkieOceny();
     }
 }
